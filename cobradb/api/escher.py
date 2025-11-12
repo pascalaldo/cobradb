844825ceb349cdc3e50755554530ffc2b896d934
Pascal A. Pieters <paspie@biosustain.dtu.dk>
2025-11-12 07:53:45 +0000

    More escher improvements

--- cobradb/api/escher.py
+++ cobradb/api/escher.py
@@ -1,9 +1,26 @@
 from functools import partial
-from typing import Any, Dict, Iterable, List, Optional, Tuple, Union
-from cobradb import get_data
+import logging
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    Iterable,
+    List,
+    Optional,
+    Tuple,
+    Union,
+    # assert_type,
+)
+from cobradb import get_data, parse
 from sqlalchemy import select
 from sqlalchemy.orm import Session
-from cobradb.models import EscherModule, ModelReaction, ReactionMatrix
+from cobradb.models import (
+    ComponentIDMapping,
+    EscherModule,
+    ModelReaction,
+    ReactionMatrix,
+    UniversalComponent,
+)
 from biggr_maps import map, pathway, template
 from pathlib import Path
 import os
@@ -33,11 +50,11 @@ class EscherModuleDefinition:
         return self._module_db
 
     def select_model_reactions(
-        self, model_reactions: Iterable[ModelReaction]
+        self, session: Session, model_reactions: Iterable[ModelReaction]
     ) -> List[ModelReaction]:
         raise NotImplementedError("Not implemented in base class.")
 
-    def build_map(self, model_reactions: Iterable[ModelReaction]):
+    def build_map(self, session: Session, model_reactions: Iterable[ModelReaction]):
         m = map.Map(
             name=self.name,
             description=self.description,
@@ -67,7 +84,7 @@ class EscherBackboneModuleDefinition(EscherModuleDefinition):
         super().__init__(**kwargs)
 
     def select_model_reactions(
-        self, model_reactions: Iterable[ModelReaction]
+        self, session: Session, model_reactions: Iterable[ModelReaction]
     ) -> List[ModelReaction]:
         selected_model_reactions = []
         for model_reaction in model_reactions:
@@ -105,8 +122,10 @@ class EscherBackboneModuleDefinition(EscherModuleDefinition):
         reactants = list(sorted(reactants, key=_reactants_sorting))
         return reactants
 
-    def build_map(self, model_reactions: Iterable[ModelReaction]) -> map.Map:
-        m = super().build_map(model_reactions)
+    def build_map(
+        self, session: Session, model_reactions: Iterable[ModelReaction]
+    ) -> map.Map:
+        m = super().build_map(session, model_reactions)
 
         backbone = {bigg_id: node.copy() for bigg_id, node in self.backbone.items()}
         for node in backbone.values():
@@ -142,38 +161,55 @@ class EscherTemplateModuleDefinition(EscherModuleDefinition):
         self.required_fraction_matching_reactions = required_fraction_matching_reactions
         super().__init__(bigg_id=bigg_id, name="Template", description="Template")
 
-    def load_template(self):
+    def load_template(self, session: Session):
         full_path = get_data("escher_templates", self.template_filename)
         with open(full_path, "r") as f:
             m, reactions, nodes = template.load_as_template(f)
+
+        if self.bigg_ids_in_template_are_universal:
+            node_bigg_ids = [
+                parse.split_compartment(node.bigg_id)[0]
+                for node in nodes.values()
+                if node.node_type == "metabolite"
+            ]
+            bigg_id_mapping_db = session.execute(
+                select(ComponentIDMapping.old_bigg_id, UniversalComponent.bigg_id)
+                .join(ComponentIDMapping.new_universal_component)
+                .filter(ComponentIDMapping.old_bigg_id.in_(node_bigg_ids))
+            ).all()
+            bigg_id_mapping = {
+                row.old_bigg_id: row.bigg_id for row in bigg_id_mapping_db
+            }
+            for node in nodes.values():
+                if node.node_type != "metabolite":
+                    continue
+                old_bigg_id, compartment = parse.split_compartment(node.bigg_id)
+                if old_bigg_id not in bigg_id_mapping:
+                    continue
+                new_bigg_id = f"{bigg_id_mapping[old_bigg_id]}_{compartment}"
+                node.bigg_id = new_bigg_id
+
         return m, reactions, nodes
 
     def select_model_reactions(
-        self, model_reactions: Iterable[ModelReaction]
+        self, session: Session, model_reactions: Iterable[ModelReaction]
     ) -> List[ModelReaction]:
-        _, reactions, _ = self.load_template()
-        selected_model_reactions = []
-        for model_reaction in model_reactions:
-            model_reaction_bigg_ids = []
-            for reaction_matrix in model_reaction.reaction.matrix:
-                if self.bigg_ids_in_template_are_universal:
-                    bigg_id = (
-                        reaction_matrix.compartmentalized_component.universal_compartmentalized_component.bigg_id
-                    )
-                else:
-                    bigg_id = reaction_matrix.compartmentalized_component.bigg_id
-                model_reaction_bigg_ids.append(bigg_id)
+        m, reactions, _ = self.load_template(session)
 
-            for reaction in reactions:
-                if reaction.finalized:
-                    continue
-                for _, metabolite in reaction.metabolites:
-                    if metabolite.bigg_id not in model_reaction_bigg_ids:
-                        break
-                else:
-                    selected_model_reactions.append(model_reaction)
-                    reaction.finalized = True
-                    break
+        selected_model_reactions = []
+
+        def _select_model_reactions(
+            mr: ModelReaction,
+            rx: map.Reaction,
+            _mr_bigg_ids: Dict[str, str],
+            _mr_matrix_dbs: Dict[str, ReactionMatrix],
+        ):
+            selected_model_reactions.append(mr)
+            rx.finalized = True
+
+        self.map_template_to_model_reactions(
+            model_reactions, m, reactions, _select_model_reactions
+        )
         if (
             len(selected_model_reactions) / len(reactions)
         ) < self.required_fraction_matching_reactions:
@@ -182,6 +218,7 @@ class EscherTemplateModuleDefinition(EscherModuleDefinition):
 
     def _build_reaction(
         self,
+        model_reaction: ModelReaction,
         reaction: map.AutoReactionWithOptionalMetabolites,
         model_reaction_bigg_ids: Dict[str, str],
         model_reaction_matrix_dbs: Dict[str, ReactionMatrix],
@@ -192,14 +229,12 @@ class EscherTemplateModuleDefinition(EscherModuleDefinition):
             bigg_id = node.bigg_id
 
             reaction_matrix = model_reaction_matrix_dbs[bigg_id]
-            new_bigg_id = model_reaction_bigg_ids[bigg_id]
 
             if (
                 new_coefficient := reaction_matrix.universal_reaction_matrix.coefficient
             ) != coefficient:
                 reaction.metabolites[i] = (new_coefficient, node)
 
-            node.bigg_id = new_bigg_id
             name = reaction_matrix.compartmentalized_component.component.name
             if name is None:
                 name = (
@@ -210,11 +245,31 @@ class EscherTemplateModuleDefinition(EscherModuleDefinition):
             del model_reaction_bigg_ids[bigg_id]
             del model_reaction_matrix_dbs[bigg_id]
 
+        absolute_sides = []
+        default_placement_opts = map.PlacementOptions()
         for bigg_id, metabolite_info in reaction.optional_metabolites.items():
+            # Calculate which side of the reaction metabolites are shown to
+            # provide smarter placement of additional metabolites.
+
+            ref_node = reaction.mid_marker
+            plus_minus = metabolite_info["coefficient"] > 0
+            if reaction.multi_markers[plus_minus] is not None:
+                ref_node = reaction.multi_markers[plus_minus]
+            _, _, _, _, _, effictive_angle_delta = reaction.calculate_placement(
+                ref_node,
+                metabolite_info["node"],
+                plus_minus,
+                0,
+                0,
+                metabolite_info.get("b1_b2"),
+                placement_opts=default_placement_opts,
+            )
+            absolute_side = (effictive_angle_delta >= 0) == bool(plus_minus)
+            absolute_sides.append(absolute_side)
+
             if bigg_id not in model_reaction_bigg_ids:
                 continue
             reaction_matrix = model_reaction_matrix_dbs[bigg_id]
-            new_bigg_id = model_reaction_bigg_ids[bigg_id]
 
             node = metabolite_info["node"]
             if (
@@ -222,7 +277,6 @@ class EscherTemplateModuleDefinition(EscherModuleDefinition):
             ) != metabolite_info["coefficient"]:
                 metabolite_info["coefficient"] = new_coefficient
 
-            node.bigg_id = new_bigg_id
             name = reaction_matrix.compartmentalized_component.component.name
             if name is None:
                 name = (
@@ -235,9 +289,15 @@ class EscherTemplateModuleDefinition(EscherModuleDefinition):
             del model_reaction_bigg_ids[bigg_id]
             del model_reaction_matrix_dbs[bigg_id]
 
+        placement_opts = None
+        if len(set(absolute_sides)) == 1:
+            placement_opts = map.PlacementOptions(
+                placement_f=partial(
+                    map.AutoReaction.same_side_placement,
+                    absolute_side=absolute_sides[0],
+                )
+            )
         for bigg_id, reaction_matrix in model_reaction_matrix_dbs.items():
-            new_bigg_id = model_reaction_bigg_ids[bigg_id]
-
             cc_db = reaction_matrix.compartmentalized_component
             name = cc_db.component.name
             if name is None:
@@ -252,12 +312,26 @@ class EscherTemplateModuleDefinition(EscherModuleDefinition):
             reaction.add_metabolite(
                 coefficient=reaction_matrix.universal_reaction_matrix.coefficient,
                 node=node,
+                placement_opts=placement_opts,
             )
+        reaction.bigg_id = model_reaction.bigg_id
         reaction.finalized = True
 
-    def build_map(self, model_reactions: Iterable[ModelReaction]) -> map.Map:
-        m, reactions, _ = self.load_template()
-
+    def map_template_to_model_reactions(
+        self,
+        model_reactions: Iterable[ModelReaction],
+        m: map.Map,
+        reactions: List[map.AutoReactionWithOptionalMetabolites],
+        build_reaction_f: Callable[
+            [
+                ModelReaction,
+                map.Reaction,
+                Dict[str, str],
+                Dict[str, ReactionMatrix],
+            ],
+            None,
+        ],
+    ) -> Dict[str, str]:
         bigg_id_mapping = {}
 
         for model_reaction in model_reactions:
@@ -268,10 +342,14 @@ class EscherTemplateModuleDefinition(EscherModuleDefinition):
                     bigg_id = (
                         reaction_matrix.compartmentalized_component.universal_compartmentalized_component.bigg_id
                     )
-                    cc_bigg_id = reaction_matrix.compartmentalized_component.bigg_id
-                    model_reaction_bigg_ids[bigg_id] = cc_bigg_id
+                    model_cc_bigg_id = reaction_matrix.compartmentalized_component.model_compartmentalized_components[
+                        0
+                    ].bigg_id
+                    model_reaction_bigg_ids[bigg_id] = model_cc_bigg_id
                 else:
-                    bigg_id = reaction_matrix.compartmentalized_component.bigg_id
+                    bigg_id = reaction_matrix.compartmentalized_component.model_compartmentalized_components[
+                        0
+                    ].bigg_id
                     model_reaction_bigg_ids[bigg_id] = bigg_id
                 model_reaction_matrix_dbs[bigg_id] = reaction_matrix
 
@@ -280,6 +358,9 @@ class EscherTemplateModuleDefinition(EscherModuleDefinition):
                     continue
                 if bigg_id_mapping[bigg_id_1] == bigg_id_2:
                     continue
+                logging.error(
+                    f"BiGG ID Clash: map BiGG ID: {bigg_id_1} -> new {bigg_id_2} (previous mapping: {bigg_id_mapping[bigg_id_1]})"
+                )
                 break
             else:
                 for reaction in reactions:
@@ -290,16 +371,35 @@ class EscherTemplateModuleDefinition(EscherModuleDefinition):
                             break
                     else:
                         bigg_id_mapping.update(model_reaction_bigg_ids)
-                        self._build_reaction(
+                        build_reaction_f(
+                            model_reaction,
                             reaction,
                             model_reaction_bigg_ids,
                             model_reaction_matrix_dbs,
                         )
                         break
+                else:
+                    logging.error(f"Could not match reaction: {model_reaction.bigg_id}")
+        return bigg_id_mapping
+
+    def build_map(
+        self, session: Session, model_reactions: Iterable[ModelReaction]
+    ) -> map.Map:
+        m, reactions, _ = self.load_template(session)
+
+        bigg_id_mapping = self.map_template_to_model_reactions(
+            model_reactions, m, reactions, self._build_reaction
+        )
+
         for reaction in reactions:
             if not reaction.finalized:
                 continue
             m.add_reaction(reaction)
+
+        for node in m.nodes.values():
+            if node.node_type == "metabolite":
+                # assert_type(node, map.MetaboliteNode)
+                node.bigg_id = bigg_id_mapping.get(node.bigg_id, node.bigg_id)
 
         return m
 
@@ -367,6 +467,7 @@ ESCHER_MODULE_DEFINITION_LIST = [
         bigg_id="central_metabolism_1",
         template_filename="iJO1366.Central metabolism.json",
         bigg_ids_in_template_are_universal=True,
+        required_fraction_matching_reactions=0.0,
     ),
 ]
 ESCHER_MODULE_DEFINITIONS = {m.bigg_id: m for m in ESCHER_MODULE_DEFINITION_LIST}
--- cobradb/escher.py
+++ cobradb/escher.py
@@ -4,7 +4,9 @@ from sqlalchemy.orm import Session, joinedload, subqueryload
 
 from cobradb.api.escher import ESCHER_MODULE_DEFINITIONS
 from cobradb.models import (
+    CompartmentalizedComponent,
     Model,
+    ModelCompartmentalizedComponent,
     ModelReaction,
     ModelReactionEscherMapping,
     Reaction,
@@ -23,14 +25,22 @@ def create_escher_modules(session: Session):
                 joinedload(ModelReaction.reaction)
                 .subqueryload(Reaction.matrix)
                 .joinedload(ReactionMatrix.compartmentalized_component)
+                .subqueryload(
+                    CompartmentalizedComponent.model_compartmentalized_components.and_(
+                        ModelCompartmentalizedComponent.model_id == model_db.id
+                    )
+                )
             )
             .filter(ModelReaction.model_id == model_db.id)
+            .order_by(ModelReaction.bigg_id)
         ).all()
 
         for escher_module in ESCHER_MODULE_DEFINITIONS.values():
             logging.warning(f"Escher Module: {escher_module.bigg_id}")
             escher_module_db = escher_module.get_or_create(session)
-            selected_reactions = escher_module.select_model_reactions(model_reactions)
+            selected_reactions = escher_module.select_model_reactions(
+                session, model_reactions
+            )
             print(f"Selected reactions: {len(selected_reactions)}")
 
             for r in selected_reactions:
